759753f8b0a0ed3aed03a800d99332d544504df3
Antovigo <antvig@protonmail.com>
2026-02-11 13:38:42 -0800

    single run

--- pythia_run.sh
+++ pythia_run.sh
@@ -38,10 +38,14 @@ cd ~/spd
 # uv run spd/experiments/mem/train_mem.py
 
 # Pythia transformer decomposition
-uv run spd/experiments/lm/lm_decomposition.py spd/experiments/lm/pythia_70m_targeted_config.yaml
+uv run spd/experiments/lm/lm_decomposition.py \
+       /mnt/nw/home/a.vigouroux/configs/pythia_seeds/pythia_70m_targeted_config.yaml
 
 # Sweep
-#uv run spd/scripts/run_variations.py spd/experiments/lm/lm_decomposition.py spd/experiments/lm/pythia_70m_targeted_config.yaml spd/experiments/lm/experimental_plan.yaml
+# uv run spd/scripts/run_variations.py \
+#        spd/experiments/lm/lm_decomposition.py \
+#        /mnt/nw/home/a.vigouroux/configs/pythia_seeds/pythia_70m_targeted_config.yaml \
+#        /mnt/nw/home/a.vigouroux/configs/pythia_seeds/experimental_plan.yaml
 
 echo "Job finished at: $(date)"
 
