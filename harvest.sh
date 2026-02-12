dfe6db4485cc9e33a6119bb829158a962e1ea1cb
Antovigo <antvig@protonmail.com>
2026-02-11 22:00:55 -0800

    a

--- harvest.sh
+++ harvest.sh
@@ -40,10 +40,7 @@ WORLD_SIZE=$SLURM_ARRAY_TASK_COUNT
 
 RUN_ID="wandb:antvig-pibbss/spd/runs/s-26988d6b"
 
-uv run python -m spd.harvest.scripts.run \
-    $RUN_ID \
-    --n_batches 1000 \
-    --use_nontarget
+uv run python -m spd.harvest.scripts.run $RUN_ID --n_batches 1000 --use_nontarget
 
 echo "Job finished at: $(date)"
 
