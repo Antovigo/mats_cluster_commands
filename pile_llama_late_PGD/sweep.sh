57858deaacfab70b32f45bfa79e3f8e84475f56e
Antovigo <antvig@protonmail.com>
2026-02-28 21:32:18 -0800

    late pgd

---
+++ pile_llama_late_PGD/sweep.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#SBATCH --job-name=late_PGD   # Name for your job
+#SBATCH --output=late_PGD_%j.log # Log file for stdout/stderr (%j = Job ID)
+#SBATCH --partition=compute           # The partition is always 'compute'
+#SBATCH --nodes=1                     # Request one node
+#SBATCH --ntasks=1                    # Request one task 
+#SBATCH --cpus-per-task=4             # Request 4 CPUs (always a good practice)
+#SBATCH --mem=32GB                    # Request 16GB of memory
+#SBATCH --gres=gpu:1                  # Request 1 L40 GPU
+#SBATCH --time=24:00:00               # Request 1 hour runtime (format: HH:MM:SS or D-HH:MM:SS)
+
+# --- To use the debug QoS, uncomment the line below ---
+# -- It has a 2-hour time limit and allows max 1 GPU.
+##SBATCH --qos=debug
+
+# --- Your job commands start here ---
+echo "========================================================"
+echo "Job Started at $(date)"
+echo "Job ID: $SLURM_JOB_ID"
+echo "Running on host: $(hostname)"
+echo "Partition: $SLURM_JOB_PARTITION"
+echo "QoS: $SLURM_JOB_QOS"
+echo "Account: $SLURM_JOB_ACCOUNT"
+echo "Allocated GPU(s): $CUDA_VISIBLE_DEVICES"
+echo "========================================================"
+echo
+
+# Load any necessary modules (e.g., conda, cuda)
+# module load cuda/12.2
+
+# echo "GPU info:"
+# nvidia-smi
+
+cd ~/SPD/spd_alt
+# uv run wandb offline
+
+# Pythia transformer training
+# uv run spd/experiments/mem/train_mem.py
+
+# Pythia transformer decomposition
+#uv run spd/experiments/lm/lm_decomposition.py spd/experiments/lm/pythia_70m_targeted_config.yaml
+
+# Sweep
+uv run spd/scripts/run_variations.py \
+       spd/experiments/lm/lm_decomposition.py \
+       /mnt/nw/home/a.vigouroux/SPD/batch_commands/pile_llama_late_PGD/pile_llama_simple_mlp-4L-targeted.yaml \
+       /mnt/nw/home/a.vigouroux/SPD/batch_commands/pile_llama_late_PGD/experimental_plan.yaml
+
+echo "Job finished at: $(date)"
+
