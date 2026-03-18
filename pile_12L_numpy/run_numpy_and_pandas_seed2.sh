fc2d69cca28d1b02a62e568871f5616a42333868
Antovigo <antvig@protonmail.com>
2026-03-18 00:51:50 -0700

    pile_12L_numpy

--- pile_12L_numpy/run_numpy_and_pandas_seed2.sh
+++ pile_12L_numpy/run_numpy_and_pandas_seed2.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32GB
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:3
 #SBATCH --time=24:00:00
 
 echo "========================================================"
