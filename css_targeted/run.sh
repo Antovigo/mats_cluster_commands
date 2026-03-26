0a0d97ed0c2d96f7797036eee5b2ca97e5b40c92
Antovigo <antvig@protonmail.com>
2026-03-26 13:43:04 -0700

    singel GPU

--- css_targeted/run.sh
+++ css_targeted/run.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=32GB
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
 
 echo "========================================================"
