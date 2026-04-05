729fd7ccbadd64f341e988a5ceb2c7a99f601071
Antovigo <antvig@protonmail.com>
2026-04-05 21:04:46 +0200

    unmasked CSS run

--- css_targeted/run_hybridize_matrices.sh
+++ css_targeted/run_hybridize_matrices.sh
@@ -10,9 +10,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=2:00:00
 
-MODEL_PATH=~/spd_out/spd/s-e790005d/model_50000.pth
+#MODEL_PATH=~/spd_out/spd/s-e790005d/model_50000.pth
+MODEL_PATH=~/spd_out/jose/model_400000.pth
 
 cd ~/SPD/spd_alt
 
 uv run python -m spd.scripts.decomposition_stress_test.hybridize_matrices "$MODEL_PATH" \
-    --n-batches 1 --batch-size 256 --n-masks 32
+    --n-batches 64 --batch-size 64 --n-masks 5 --scale log
