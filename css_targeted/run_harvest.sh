a3a2ec031bb210b91385e4a145f4b7d2572df763
Antovigo <antvig@protonmail.com>
2026-04-08 22:33:28 +0200

    tmp

--- css_targeted/run_harvest.sh
+++ css_targeted/run_harvest.sh
@@ -2,5 +2,7 @@
 
 cd ~/SPD/spd_alt
 
-export TMPDIR=/ephemeral/$USER
+# export TMPDIR=/ephemeral/$USER
+export TMPDIR=/mnt/nw/home/a.vigouroux
+
 uv run spd-harvest /mnt/nw/home/a.vigouroux/SPD/batch_commands/css_targeted/harvest_config.yaml
