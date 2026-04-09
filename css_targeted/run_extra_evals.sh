b086e2ff67c592e38b5d457cbde5c7505f1c64ce
Antovigo <antvig@protonmail.com>
2026-04-09 13:45:57 +0200

    extra evals on cluster

--- css_targeted/run_extra_evals.sh
+++ css_targeted/run_extra_evals.sh
@@ -8,15 +8,16 @@
 #SBATCH --output=/mnt/polished-lake/artifacts/mechanisms/spd/slurm_logs/slurm-%j.out
 
 #MODEL_PATH=~/spd_out/spd/s-e790005d/model_50000.pth
-MODEL_PATH=~/Documents/MATS/spd_out/spd/s-429ea112/model_50000.pth
+#MODEL_PATH=~/Documents/MATS/spd_out/spd/s-429ea112/model_50000.pth
+MODEL_PATH=~/spd_out/spd/s-429ea112/model_50000.pth
 
-cd ~/Code/SPD/spd_alt
+cd ~/SPD/spd_alt
 
 uv run python -m spd.scripts.decomposition_stress_test.recon_distribution "$MODEL_PATH" \
-    --n-batches 512 --batch-size 2
+    --n-batches 512 --batch-size 16
 
 uv run python -m spd.scripts.decomposition_stress_test.per_matrix_recon "$MODEL_PATH" \
-    --n-batches 256 --batch-size 2
+    --n-batches 256 --batch-size 16
 
 uv run python -m spd.scripts.decomposition_stress_test.entropy_vs_metrics "$MODEL_PATH" \
-    --n-batches 128 --batch-size 2
+    --n-batches 128 --batch-size 16
