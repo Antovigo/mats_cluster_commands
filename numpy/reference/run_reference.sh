4c404785749ddf9228724c83df86a29e7f35b18d
Antovigo <antvig@protonmail.com>
2026-04-10 19:00:31 +0200

    fullPGD10x

--- numpy/reference/run_reference.sh
+++ numpy/reference/run_reference.sh
@@ -22,6 +22,6 @@ echo
 cd ~/SPD/spd
 
 uv run spd/experiments/lm/lm_decomposition.py \
-  /mnt/nw/home/a.vigouroux/SPD/batch_commands/numpy/reference/config_numpy_reference_fullPGD.yaml
+  /mnt/nw/home/a.vigouroux/SPD/batch_commands/numpy/reference/config_numpy_reference_fullPGD_10x.yaml
 
 echo "Job finished at: $(date)"
